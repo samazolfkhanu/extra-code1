90e0d179696460334be2e327bc26ec257ee6f709
samazolfkhanu <samazolfkhani0@gmail.com>
2025-02-25 06:50:27 -0800

    print numbers in reverse

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        /*System.out.print("enter a number: ");
+        int num=scan.nextInt();
+        int b=num,j=num;
+        for(int i=0;i<num;i++)
+        {
+            while(j>0)
+            {
+                System.out.print(j);
+                j--;
+            }
+            System.out.print("\n");
+            b--;
+            j=b;
+        }
+        scan.close();*/
+        
+    }
+}
